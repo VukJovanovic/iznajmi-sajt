5b8450bedd930bb42974b03522c0fcc12ec124b5
Vuk Jovanovic <vukjovanovic34@gmail.com>
2020-08-08 19:39:28 +0200

    updejtovi

--- css/main.js
+++ css/main.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57680" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58230" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
